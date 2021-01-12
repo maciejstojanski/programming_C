4cf5a95a26bad7985b39f3f6d14242a1364d1071
maciejstojanski <stojanskiamciej@gmail.com>
2021-01-12 20:25:51 +0100

    test

---
+++ maic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    char* przedmiot= "koszulka adidas";
+
+    printf ("%s\n\n", przedmiot);
+    printf(" napis zajmuje wiecej niz 1 linijke \
+    i sprawdzamy czy mozna go zapisac w wiecej niz 1 linijce\
+    i zobcze czy to ma sens");
+
+    return 0;}
